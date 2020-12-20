30f478ac33081dd9b4709b65880c1a67fc37ba1d
Jothamsl <jothamsl1@gmail.com>
2020-12-20 22:32:52 +0100

    Finished up Primitive Basics & added built in functions

---
+++ Data types & structures/functions.py
@@ -0,0 +1,33 @@
+# ----------
+#    MATH
+# ----------
+# max()       Returns the largest of the given arguments or items in an iterable 
+# min()       Returns the smallest of the given arguments or items in an iterable 
+# pow()       Raises the smallest of the given arguments or items in an iterable 
+# round()     Rounds a floating-point value
+# sum()       Sums the items of an iterable 
+
+# -------------------
+#   TYPE CONVERSION
+# -------------------
+# ascii()     Returns a string containing a printable representation of an object 
+# bin()       Converts an integer to a binary string 
+# bool()      Converts an argument to a Boolean value 
+# chr()       Returns string representation of character given by integer argument 
+# complex()   Returns a comlex number constructed from arguments 
+# float()     Returns a floating-point object constructed from a number or string
+# hex()       Converts an integer to a hexadecimal string 
+# int()       Returns an integer object constructed from a number or string 
+# oct()       Converts an integer to an octual string
+# ord()       Returns integer representation of a character 
+# repr()      Returns a string containing a printable representation of an object 
+# str()       Returns a string verion of an object 
+# type()      Returns the type of an object or creates a new type object 
+
+# -------------------------
+#  ITERABLES AND ITERATORS
+# -------------------------
+# all()       Returns True if all elements of an iterable are true 
+# any()       Returns True if any elements of an iterable are true 
+# enumerate() Returns
+# filter()    Filters elements from an iterable 
