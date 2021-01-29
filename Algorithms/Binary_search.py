2f34682522ebecccefde0e4be577bd1d14479109
Jothamsl <jothamsl1@gmail.com>
2021-01-29 02:43:17 +0100

    Changed Binary_sort function name to Binary_search

--- Algorithms/Binary_search.py
+++ Algorithms/Binary_search.py
@@ -2,17 +2,18 @@ def main():
     search_val = 8
     arr = [1, 3, 5, 7, 8]
     high = len(arr) - 1
-    binary_sort(arr, search_val, 0, high)
+    binary_search(arr, search_val, 0, high)
 
-def binary_sort(arr, val, low, high):
+
+def binary_search(arr, val, low, high):
     # Define base case
     if val == arr[high]:
         print(f'{val} found in index {high}')
 
     # Recursively search list
     if val > high:
-        binary_sort(arr, val, high, arr[-1])
-    
+        binary_search(arr, val, high, arr[-1])
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     main()
