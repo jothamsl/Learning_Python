171bd16dc3d5cedfd92d5d54a96e5c59694a102a
Jothamsl <jothamsl1@gmail.com>
2021-02-22 20:24:03 +0100

    Updated Built in functions

--- Data types & structures/Functions/Built_in_functions.py
+++ Data types & structures/Functions/Built_in_functions.py
@@ -1,33 +1,37 @@
 # ----------
 #    MATH
 # ----------
-# max()       Returns the largest of the given arguments or items in an iterable 
-# min()       Returns the smallest of the given arguments or items in an iterable 
-# pow()       Raises the smallest of the given arguments or items in an iterable 
+# max()       Returns the largest of the given arguments or items in an iterable
+# min()       Returns the smallest of the given arguments or items in an iterable
+# pow()       Raises the smallest of the given arguments or items in an iterable
 # round()     Rounds a floating-point value
-# sum()       Sums the items of an iterable 
+# sum()       Sums the items of an iterable
+# abs()       Returns the absolute value of a number
+# complex()   Returns a complex number
 
 # -------------------
 #   TYPE CONVERSION
 # -------------------
-# ascii()     Returns a string containing a printable representation of an object 
-# bin()       Converts an integer to a binary string 
-# bool()      Converts an argument to a Boolean value 
-# chr()       Returns string representation of character given by integer argument 
-# complex()   Returns a comlex number constructed from arguments 
+# ascii()     Returns a string containing a printable representation of an object
+# bin()       Converts an integer to a binary string
+# bool()      Converts an argument to a Boolean value
+# chr()       Returns string representation of character given by integer argument
+# complex()   Returns a comlex number constructed from arguments
 # float()     Returns a floating-point object constructed from a number or string
-# hex()       Converts an integer to a hexadecimal string 
-# int()       Returns an integer object constructed from a number or string 
+# hex()       Converts an integer to a hexadecimal string
+# int()       Returns an integer object constructed from a number or string
 # oct()       Converts an integer to an octual string
-# ord()       Returns integer representation of a character 
-# repr()      Returns a string containing a printable representation of an object 
-# str()       Returns a string verion of an object 
-# type()      Returns the type of an object or creates a new type object 
+# ord()       Returns integer representation of a character
+# repr()      Returns a string containing a printable representation of an object
+# str()       Returns a string verion of an object
+# type()      Returns the type of an object or creates a new type object
+# ascii()     Returns String containing printable representation
 
 # -------------------------
 #  ITERABLES AND ITERATORS
 # -------------------------
-# all()       Returns True if all elements of an iterable are true 
-# any()       Returns True if any elements of an iterable are true 
+# all()       Returns True if all elements of an iterable are true (uses && each)
+# any()       Returns True if any elements of an iterable are true (uses || each)
 # enumerate() Returns
-# filter()    Filters elements from an iterable 
+# filter()    Filters elements from an iterable
+# bytearray() Returns an array of given byte size 
